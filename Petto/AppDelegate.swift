457d93d43d7a1ba4f2cc7f7313ffab65923c0af4
a-sueki <sueki.aoi@gmail.com>
2017-08-26 09:04:25 +0900

    通知バッチの表示、ボタン動作 -> OK

--- Petto/AppDelegate.swift
+++ Petto/AppDelegate.swift
@@ -9,9 +9,10 @@
 import UIKit
 import Firebase
 import SlideMenuControllerSwift
+import UserNotifications
 
 @UIApplicationMain
-class AppDelegate: UIResponder, UIApplicationDelegate {
+class AppDelegate: UIResponder, UIApplicationDelegate, UNUserNotificationCenterDelegate {
 
     var window: UIWindow?
     var myNavigationController: UINavigationController?
@@ -37,9 +38,21 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
         //self.window?.backgroundColor = UIColor.red
         self.window?.rootViewController = slideMenuController
         self.window?.makeKeyAndVisible()
-                    
+        
+        // ユーザに通知の許可を求める
+        let center = UNUserNotificationCenter.current()
+        center.requestAuthorization(options: [.alert, .sound]) { (granted, error) in
+            // Enable or disable features based on authorization
+        }
+        center.delegate = self;     // 追加
+        
         print("DEBUG_PRINT: AppDelegate.application end ")
         return true
+    }
+    
+    // アプリがフォアグラウンドの時に通知を受け取ると呼ばれるメソッド
+    func userNotificationCenter(_ center: UNUserNotificationCenter, willPresent notification: UNNotification, withCompletionHandler completionHandler: @escaping (UNNotificationPresentationOptions) -> Void) {
+        completionHandler([.sound, .alert])
     }
 
     func applicationWillResignActive(_ application: UIApplication) {
--- Petto/BaseViewController.swift
+++ Petto/BaseViewController.swift
@@ -11,6 +11,7 @@ import Firebase
 import FirebaseAuth
 import FirebaseDatabase
 import RKNotificationHub
+import UserNotifications
 
 class BaseViewController: UIViewController {
 
@@ -29,44 +30,22 @@ class BaseViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         print("DEBUG_PRINT: BaseViewController.viewDidLoad start")
-
-        // UiView を バーボタンにセットして作成
-        // UIButton をバーボタンに設定
-        // UIButtonを作成
-//        let view4 = UIView(frame: CGRect(x: 0, y: 0, width: 25, height: 25))
-//        let image4 = UIImageView(image: UIImage(named: "mail"))
-//       view4.addSubview(image4)
-        let button4 = UIButton()
+        
+        // 通知バッチを表示
+        let button4 = UIButton(frame: CGRect(x: 0, y: 0, width: 25, height: 25))
         button4.setImage(UIImage(named: "mail"), for: .normal)
         button4.addTarget(self, action: #selector(self.onClick4), for: .touchUpInside)
-//        view4.addSubview(button4)
-        // ボタンタップ時のアクション設定
-        //view.target(forAction: #selector(self.onClick4), withSender: self)
-//        button.addTarget(self, action: #selector(self.onClick4), for:UIControlEvents.touchUpInside)
-        // UIButton を バーボタンに設定
-        //let btn4 : UIBarButtonItem = UIBarButtonItem(customView: button)
+        //  hubをhogeButtonに設置し、最初に表示させるバッジのCountを3にする
+        hub.setView(button4, andCount: 3)
+        //  設置するhubの背景色を黒に文字色を白にする
+        hub.setCircleColor(UIColor.black, label: UIColor.white)
 
         btn1 = UIBarButtonItem(image: UIImage(named: "menu"), style: .plain, target: self, action: #selector(self.onClick1))
         btn2 = UIBarButtonItem(image: UIImage(named: "logo"), style: .plain, target: self, action: #selector(self.onClick2))
         btn3 = UIBarButtonItem(image: UIImage(named: "todolist"), style: .plain, target: self, action: #selector(self.onClick3))
-//        btn4 = UIBarButtonItem(image: UIImage(named: "mail"), style: .plain, target: self, action: #selector(self.onClick4))
         btn4 = UIBarButtonItem(customView: button4)
-
         btn5 = UIBarButtonItem(image: UIImage(named: "search"), style: .plain, target: self, action: #selector(self.onClick5))
 
-        
-        
-
-        
-        
-        //  hubをhogeButtonに設置し、最初に表示させるバッジのCountを3にする
-        print("DEBUG_PRINT: BaseViewController.viewDidLoad 1")
-//        hub.setView(view4, andCount: 3)
-//        hub.setView(btn4, andCount: 3)
-        //  設置するhubの背景色を黒に文字色を白にする
-//        hub.setCircleColor(UIColor.black, label: UIColor.white)
-        
-        
         let leftBtns: [UIBarButtonItem] = [btn1,btn2]
         let rightBtns: [UIBarButtonItem] = [btn3,btn4,btn5]
         
@@ -75,11 +54,52 @@ class BaseViewController: UIViewController {
         
         print("DEBUG_PRINT: BaseViewController.viewDidLoad end")
     }
+    
+/*    override func viewWillAppear(_ animated: Bool) {
+        //var message = MessageData(snapshot: <#FIRDataSnapshot#>)
+        //setNotification(message: message)
+        setNotification(s: "aaaaa")
+    }
 
+    func setNotification(s: String) {
+        print("DEBUG_PRINT: BaseViewController.setNotification start")
+        let content = UNMutableNotificationContent()
+        content.title = "たいとる"
+        content.body = s
+        content.sound = UNNotificationSound.default()
+        
+        // ローカル通知が発動するtrigger（日付マッチ）を作成
+        let calendar = NSCalendar.current
+        let dateComponents = calendar.dateComponents([.year, .month, .day, .hour, .minute], from: Date() as Date)
+        let trigger = UNCalendarNotificationTrigger.init(dateMatching: dateComponents, repeats: false)
+//        let tigger = UNPushNotificationTrigger
+        
+        // identifier, content, triggerからローカル通知を作成（identifierが同じだとローカル通知を上書き保存）
+        let request = UNNotificationRequest.init(identifier: String("xxx"), content: content, trigger: trigger)
+        
+        
+        // ローカル通知を登録
+        let center = UNUserNotificationCenter.current()
+        center.add(request) { (error) in
+            print(error ?? "ローカル通知登録 OK")  // error が nil ならローカル通知の登録に成功したと表示します。errorが存在すればerrorを表示します。
+        }
+        
+        // 未通知のローカル通知一覧をログ出力
+        center.getPendingNotificationRequests { (requests: [UNNotificationRequest]) in
+            for request in requests {
+                print("/---------------")
+                print(request)
+                print("---------------/")
+            }
+        }
+        print("DEBUG_PRINT: BaseViewController.setNotification start")
+    }
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
+*/
     
 
     func onClick1() {
@@ -101,6 +121,4 @@ class BaseViewController: UIViewController {
         let viewController5 = self.storyboard?.instantiateViewController(withIdentifier: "Search") as! SearchViewController
         self.navigationController?.pushViewController(viewController5, animated: true)
     }
-
-
 }
